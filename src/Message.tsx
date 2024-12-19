a947d792c8444f88530240b915fc8909bc2e1ed9
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 08:51:54 +0600

    name checking with if

--- src/Message.tsx
+++ src/Message.tsx
@@ -3,8 +3,9 @@
 function Message() {
   // JSX: JavaScript XML
   const name = 'Tanjir';
-  return <h1>Hello {name}</h1>;
- // return <h1>Hello World</h1>;
+  if(name)
+    return <h1>Hello {name}</h1>;
+ return <h1>Hello World</h1>;
 }
 
 export default Message;
