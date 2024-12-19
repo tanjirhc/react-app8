cea77edb3b3599ae56d779fa7086ae8996335adf
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 21:54:16 +0600

    selectedIndex

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,6 +2,7 @@ import { MouseEvent } from "react";
 
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const selectedIndex = 0;
 
   // Event handler
   const handleClick = (event: MouseEvent) => console.log(event);
@@ -11,12 +12,8 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>NO item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={handleClick}
-          >
+        {items.map((item, index) => (
+          <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
