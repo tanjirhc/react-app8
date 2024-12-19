5bb5d039d8913aa5d7a7bbf036dddbf11dc06b73
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 22:06:02 +0600

    setSelectedIndex

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,13 +2,8 @@ import { useState } from "react";
 
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  let selectedIndex = 0;
-  // Hook
-  const [selectedIndex, setSelectedIndex]= useState(-1);
-  arr[0] // variable (selectedIndex)
-  arr[1] // updater function
-  const [name, setName] = useState('');
 
+  const [selectedIndex, setSelectedIndex]= useState(-1);
 
 
   return (
@@ -17,7 +12,7 @@ function ListGroup() {
       {items.length === 0 && <p>NO item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={() => { selectedIndex = index;}}>
+          <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={() => { setSelectedIndex(index); }}>
             {item}
           </li>
         ))}
