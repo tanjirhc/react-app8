45069c4aaa388bf4dff3d0ab29d61554a9eb2bbc
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 09:16:33 +0600

    code formatted

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,14 @@
-
 function ListGroup() {
-  return <h1>List Group</h1>;
+  return (
+  <ul className="list-group">
+    <li className="list-group-item disabled" aria-disabled="true">
+      A disabled item
+    </li>
+    <li className="list-group-item">A second item</li>
+    <li className="list-group-item">A third item</li>
+    <li className="list-group-item">A fourth item</li>
+    <li className="list-group-item">And a fifth one</li>
+  </ul>);
 }
 
 export default ListGroup;
