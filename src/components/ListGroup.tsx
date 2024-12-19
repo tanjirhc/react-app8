db2bdfc8b8cfb1a2e6805c394521f1b08ce6e1a6
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 09:19:42 +0600

    wrapping element

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,18 @@
 function ListGroup() {
   return (
-  <ul className="list-group">
-    <li className="list-group-item disabled" aria-disabled="true">
-      A disabled item
-    </li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>);
+    <div>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item disabled" aria-disabled="true">
+          A disabled item
+        </li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
+  );
 }
 
 export default ListGroup;
