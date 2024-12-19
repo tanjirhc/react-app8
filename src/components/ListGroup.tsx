b9f89b9de2ae77837230985bf510d343975c5012
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 21:43:20 +0600

    event handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
 function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   return (
     <>
-      <h1>List</h1>      
+      <h1>List</h1>
       {items.length === 0 && <p>NO item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
