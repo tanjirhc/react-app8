9cadea9ad75a6c32510b709c8dd45bbf85599753
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 21:25:25 +0600

    inside component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,16 +2,11 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
+  
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
