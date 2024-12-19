796406595caf7c79f1edabd06501cb3d21eeb885
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-19 08:50:22 +0600

    my name print

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Message from "./Message";
 
 function App() {
+  
   return <div><Message /></div>
 }
 
--- src/Message.tsx
+++ src/Message.tsx
@@ -2,7 +2,9 @@
 
 function Message() {
   // JSX: JavaScript XML
-  return <h1>Hello World</h1>;
+  const name = 'Tanjir';
+  return <h1>Hello {name}</h1>;
+ // return <h1>Hello World</h1>;
 }
 
 export default Message;
